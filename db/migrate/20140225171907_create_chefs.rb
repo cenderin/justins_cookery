d1f6676a999e1b30bcb8fbb3f13d7852e1a54fa1
cenderin <ml.alexander87@gmail.com>
2014-02-25 09:37:27 -0800

    removed and added controllers and models

--- db/migrate/20140225171907_create_chefs.rb
+++ db/migrate/20140225171907_create_chefs.rb
@@ -1,6 +1,10 @@
 class CreateChefs < ActiveRecord::Migration
   def change
     create_table :chefs do |t|
+    	t.string	:avatar
+    	t.string :show
+    	t.string :name
+    	t.string :speciality
 
       t.timestamps
     end
