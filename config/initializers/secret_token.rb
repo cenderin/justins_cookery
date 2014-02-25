19bd18b95a8e5ab9de41842c1b76225e857a7296
cenderin <ml.alexander87@gmail.com>
2014-02-25 09:21:48 -0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JustinBrazilianCookery::Application.config.secret_key_base = 'e5551f909dd733420063ca0d891f79100ee51fc0333ffa99ae4764de7afa7be804e0ad28a8df70143a6f5792a89896652f272cbef3aeae63160b8bdd23c656d1'
