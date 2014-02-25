9eb12b58bffda5e6878dd5837680c8ebf9213a17
cenderin <ml.alexander87@gmail.com>
2014-02-25 12:17:10 -0800

    added views for recipe and chefs

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,44 @@
 class ChefsController < ApplicationController
+  def index
+    @chefs = Chef.all
+  end
 	def new
 		@chef = Chef.new
 	end
+
+	def show
+		@chef = Chef.find(params[:id])
+	end
+
+	def edit
+		@chef = Chef.find(params[:id])
+	end
+
+  def create
+    @chef = Chef.new(chef_params)
+
+      if @chef.save
+        flash[:success] = "New chef has been created."
+       	redirect_to chef_path(@chef.id)
+      else
+        flash[:error] = "There was an error saving your form."
+       	render :new
+      end
+  end
+  def update
+    @chef = Chef.find(params[:id])
+       if @chef.update_attributes(chef_params)
+        flash[:success] = "changes have been made"
+        redirect_to chef_path(@chef.id)
+       else
+        flash[:error] = "you are a failure and no one loves you"
+        render :edit
+
+       end
+  end
+
+  private
+  def chef_params
+      params.require(:chef).permit(:name, :show, :speciality, :avatar)
+    end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,46 @@
 class RecipesController < ApplicationController
+
+	def index
+    @recipes = Recipe.all
+  end
+
+	def new
+		@recipe = Recipe.new
+	end
+
+	def show
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def edit
+		@recipe = Recipe.find(params[:id])
+	end
+
+  def create
+    @Recipe = Recipe.new(recipe_params)
+
+      if @recipe.save
+        flash[:success] = "New Recipe has been created."
+       	redirect_to recipe_path(@recipe.id)
+      else
+        flash[:error] = "There was an error saving your form."
+       	render :new
+      end
+  end
+  def update
+    @recipe = Recipe.find(params[:id])
+       if @recipe.update_attributes(recipe_params)
+        flash[:success] = "changes have been made"
+        redirect_to recipe_path(@recipe.id)
+       else
+        flash[:error] = "you are a failure and no one loves you"
+        render :edit
+
+       end
+  end
+
+  private
+  def recipe_params
+      params.require(:recipe).permit(:title, :description, :cook_time, :instructions, :picture, :ingredients)
+    end
 end
